c0dd51529615b02b94824ddae854681e72256367
Owlfx <nirvaan.kohli@gmail.com>
2025-04-28 16:40:30 -0400

    home + work pages

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html")
+
 @app.route('/works')
 def works():
-    return render_template("works.html")
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
+    return render_template("works - home.html")
 
+@app.route('/about')
+def works():
+    return render_template("about - home.html")
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)  # Set debug=False for production
