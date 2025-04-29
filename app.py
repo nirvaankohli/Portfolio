3233b2aa03ac07ff6f7ebc69d40bedc9b1d76121
Owlfx <nirvaan.kohli@gmail.com>
2025-04-29 16:22:19 -0400

    final edits before

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, send_from_directory
 import json
 import os
 
 app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def home():
@@ -13,8 +18,8 @@ def works():
     return render_template("works - home.html")
 
 @app.route('/about')
-def works():
+def about():
     return render_template("about - home.html")
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)  # Set debug=False for production
+    app.run(debug=False)  # Set debug=False for production
